9bee7a49a2b71f241cc8f0607636f236ace007e7
MasterChief-John-117 <MasterChief-John-117@users.noreply.github.com>
2018-03-12 23:28:54 -0700

    made autosync recurring

--- src/com/chef/Main.java
+++ src/com/chef/Main.java
@@ -7,13 +7,12 @@ class Main
     public static final xkcdClient xkcd = new xkcdClient();
 
     public static void main(String[] args) {
-        xkcd.sync();
         Timer autoSync = new Timer();
-        autoSync.schedule(new TimerTask() {
+        autoSync.scheduleAtFixedRate(new TimerTask() {
                     @Override
                     public void run() {
                         xkcd.sync();
                     }
-                }, 30 * 60 * 1000);
+                }, new Date(), 30 * 60 * 1000);
     }
 }
