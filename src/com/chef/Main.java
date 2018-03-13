2d7aea09375e40941480650a08d356362072cc6f
MasterChief-John-117 <MasterChief-John-117@users.noreply.github.com>
2018-03-13 00:25:55 -0700

    spark web app for title search

--- src/com/chef/Main.java
+++ src/com/chef/Main.java
@@ -1,11 +1,14 @@
 package com.chef;
 
+import com.google.gson.*;
 import java.util.*;
 import static spark.Spark.*;
 
 class Main
 {
     public static final xkcdClient xkcd = new xkcdClient();
+    public static final int port = 80;
+    public static Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
     public static void main(String[] args) {
         //Start by configuring automatic syncing of comics to run every 30 minutes
@@ -16,5 +19,14 @@ class Main
                         xkcd.sync();
                     }
                 }, new Date(), 30 * 60 * 1000);
+
+        System.out.println("Setting port to " + port);
+        port(port);
+
+        get("/title/:title", ((request, response) -> {
+            String title = request.params(":title").replaceAll("%20", " ");
+            response.type("application/json");
+            return gson.toJson(xkcd.searchByTitle(title));
+        }));
     }
 }
