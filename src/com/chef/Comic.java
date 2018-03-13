6ecdd63e5f2823f3f994ab17419210065cfabddf
MasterChief-John-117 <MasterChief-John-117@users.noreply.github.com>
2018-03-12 20:38:03 -0700

    +comic class

---
+++ src/com/chef/Comic.java
@@ -0,0 +1,19 @@
+package com.chef;
+
+import java.io.IOException;
+import java.net.*;
+import java.util.*;
+import com.google.gson.*;
+
+public class Comic
+{
+    public int num;
+    public String title;
+    public String safe_title;
+    public String alt;
+    public String img;
+    public String year;
+    public String month;
+    public String day;
+
+}
