7a35794792e52745f68b9c9505d3f53959a593c5
MasterChief-John-117 <MasterChief-John-117@users.noreply.github.com>
2018-03-12 23:09:30 -0700

    sync

--- src/com/chef/Main.java
+++ src/com/chef/Main.java
@@ -1,8 +1,15 @@
 package com.chef;
 
-public class Main {
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Main
+{
+    public static final xkcdClient xkcd = new xkcdClient();
 
     public static void main(String[] args) {
-
+        xkcd.sync();
     }
 }
--- src/com/chef/xkcdClient.java
+++ src/com/chef/xkcdClient.java
@@ -1,9 +1,83 @@
 package com.chef;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.*;
+import java.util.*;
+
 public class xkcdClient {
     public final String latestUrl = "https://xkcd.com/info.0.json";
+    private final String cachePath = "comics.json";
+    private List<Comic> fullCache = new ArrayList<>();
     public String getApiUrlFromNumber(int num)
     {
         return "https://xkcd.com/" + num + "/info.0.json";
+    }
+
+    public void sync() {
+        System.out.println("Starting sync at " + new Date().toString());
+        File cacheFile = new File(cachePath);
+        try {
+            cacheFile.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        StringBuilder sb = new StringBuilder();
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader(cacheFile));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+        String line = null;
+        try
+        {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+            }
+        }
+        catch(Exception ex)
+        {ex.printStackTrace();}
+        String rawCache = sb.toString();
+
+        if (rawCache.length() < 2) {
+            fullCache = new ArrayList<Comic>();
+        }
+        else
+        {
+            fullCache = gson.fromJson(rawCache, new TypeToken<ArrayList<Comic>>(){}.getType());
+        }
+
+        int next;
+        if (fullCache.isEmpty())
+        {
+            next = 1;
+        }
+        else
+        {
+            next = fullCache.size() + 1;
+        }
+
+        int addedCount = 0;
+        Comic lastComic = new Comic(latestUrl);
+        System.out.println("Latest comic is " + lastComic.num + ", last stored is " + (fullCache.size()));
+        for(int i = next; i <= lastComic.num; i++)
+        {
+            System.out.println("Adding comic " + i);
+            fullCache.add(new Comic(getApiUrlFromNumber(i)));
+            addedCount++;
+        }
+
+        try (PrintWriter out = new PrintWriter(cacheFile.getName())) {
+            out.println(gson.toJson(fullCache));
+            System.out.println("Sync finished at " + new Date() + "\nAdded " + addedCount + " comics");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
