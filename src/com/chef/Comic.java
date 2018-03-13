41af50f2358b95583dd326146bd28dc2118e396c
MasterChief-John-117 <MasterChief-John-117@users.noreply.github.com>
2018-03-12 20:38:20 -0700

    +Get Comic from URL

--- src/com/chef/Comic.java
+++ src/com/chef/Comic.java
@@ -16,4 +16,32 @@ public class Comic
     public String month;
     public String day;
 
+    public Comic(String Url)
+    {
+        try
+        {
+            URL url = new URL(Url);
+            Scanner scanner = new Scanner(url.openStream());
+            StringBuilder sb = new StringBuilder();
+            while(scanner.hasNextLine())
+            {
+                sb.append(scanner.nextLine());
+            }
+            String jsonValue = sb.toString();
+            
+            Gson gson = new Gson();
+            Comic newC = gson.fromJson(jsonValue, Comic.class);
+
+            this.num = newC.num;
+            this.title = newC.title;
+            this.safe_title = newC.safe_title;
+            this.alt = newC.alt;
+            this.img = newC.img;
+            this.year = newC.year;
+            this.month = newC.month;
+            this.day = newC.day;
+        }
+        catch(IOException IOEx)
+        {IOEx.printStackTrace();}
+    }
 }
